ab7993f5995bce562d21a8ce7442bb73a7e68f94
“[Antti <[antti.ht.kosonen@gmail.com]>
2018-11-04 21:58:03 +0200

    Frontend rebuilt

---
+++ build/precache-manifest.b40dbb98520813e2a43845ea0cfff658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6fb7431228a47d8b1ed9",
+    "url": "/static/js/main.6fb74312.chunk.js"
+  },
+  {
+    "revision": "600b755f42a39315c72a",
+    "url": "/static/js/1.600b755f.chunk.js"
+  },
+  {
+    "revision": "6fb7431228a47d8b1ed9",
+    "url": "/static/css/main.efe02a30.chunk.css"
+  },
+  {
+    "revision": "c27a9306f25fb3f6488259beb3015d8a",
+    "url": "/index.html"
+  }
+];
